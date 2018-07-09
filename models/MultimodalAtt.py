4fec54972f62f490746093078cf5f46b089842b3
Chenxiao Guan <1011zaozao@gmail.com>
2018-07-09 11:35:22 -0400

    m1

--- models/MultimodalAtt.py
+++ models/MultimodalAtt.py
@@ -50,8 +50,9 @@ class MultimodalAtt(nn.Module):
         decoder_c0 = video_cell_state + audio_cell_state
 
         decoder_input = pad_sequence([audio_encoder_output.squeeze(), video_encoder_output.squeeze()])
-        decoder_input = torch.transpose(decoder_input, 0, 1)
+        decoder_input = torch.transpose(decoder_input, 1, 2)
         decoder_input = self.fuse_input(decoder_input)
+        decoder_input = decoder_input.suqeeze().unsqueeze(0)
 
         decoder_output, (decoder_hidden, decoder_cell) = self.decoder(decoder_input, (decoder_h0, decoder_c0))
         seq_probs = list()
