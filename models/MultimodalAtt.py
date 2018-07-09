42a375b75fd65679a1eb7a4fdece80e04c41fd95
Chenxiao Guan <1011zaozao@gmail.com>
2018-07-09 11:42:21 -0400

    cuda

--- models/MultimodalAtt.py
+++ models/MultimodalAtt.py
@@ -42,8 +42,8 @@ class MultimodalAtt(nn.Module):
     def forward(self, image_feats, audio_feats, target_variable=None, mode='train', opt={}):
         batch_size, n_frames, _ = image_feats.shape
         _, n_mfcc, __ = audio_feats.shape
-        padding_words = torch.zeros((batch_size, n_frames if n_frames>n_mfcc else n_mfcc, self.dim_word))
-        padding_frames = torch.zeros((batch_size, 1, self.dim_vid))
+        padding_words = torch.zeros((batch_size, n_frames if n_frames>n_mfcc else n_mfcc, self.dim_word)).cuda()
+        padding_frames = torch.zeros((batch_size, 1, self.dim_vid)).cuda()
         video_encoder_output, (video_hidden_state, video_cell_state) = self.video_rnn_encoder(image_feats)
         audio_encoder_output, (audio_hidden_state, audio_cell_state) = self.audio_rnn_encoder(audio_feats)
         decoder_h0 = torch.cat((video_hidden_state, audio_hidden_state), dim=2)
