8097a5ad3377d939faa14f219b7898902f4574e1
Chenxiao Guan <1011zaozao@gmail.com>
2018-07-10 16:22:57 -0400

    d

--- models/MultimodalAtt.py
+++ models/MultimodalAtt.py
@@ -2,8 +2,8 @@ import torch
 import torch.nn as nn 
 import torch.nn.functional as F
 from torch.nn.utils.rnn import pad_sequence
-from Attention import Attention
-from ChildSum import ChildSum
+from .Attention import Attention
+from .ChildSum import ChildSum
 
 class MultimodalAtt(nn.Module):
 
