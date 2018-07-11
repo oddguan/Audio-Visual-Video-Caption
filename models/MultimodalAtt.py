256bb02655106114f9e7622928da1153ebddf418
Chenxiao Guan <1011zaozao@gmail.com>
2018-07-10 20:03:53 -0400

    test

--- models/MultimodalAtt.py
+++ models/MultimodalAtt.py
@@ -53,8 +53,8 @@ class MultimodalAtt(nn.Module):
         audio_encoder_output, (audio_hidden_state, audio_cell_state) = self.audio_rnn_encoder(audio_feats)
         decoder_hidden, decoder_cell = self.ChildSum(video_hidden_state, audio_hidden_state, 
         video_cell_state, audio_cell_state)
-        vid_context = self.TemporalAttention_vid(decoder_hidden.unsqueeze(0), video_encoder_output)
-        aud_context = self.TemporalAttention_aud(decoder_hidden.unsqueeze(0), audio_encoder_output)
+        vid_context = self.TemporalAttention_vid(decoder_hidden, video_encoder_output)
+        aud_context = self.TemporalAttention_aud(decoder_hidden, audio_encoder_output)
         context = torch.cat((vid_context, aud_context), dim=1)
         decoder_input = self.MultiModelAttention(decoder_hidden, context)
         # decoder_input = pad_sequence([audio_encoder_output.squeeze(), video_encoder_output.squeeze()])
@@ -78,7 +78,7 @@ class MultimodalAtt(nn.Module):
                 vid_context = self.TemporalAttention_vid(decoder_hidden.unsqueeze(0), video_encoder_output)
                 aud_context = self.TemporalAttention_aud(decoder_hidden.unsqueeze(0), audio_encoder_output)
                 context = torch.cat((vid_context, aud_context), dim=1)
-                decoder_input = self.MultiModelAttention(decoder_hidden, context)
+                decoder_input = self.MultiModelAttention(decoder_hidden.unsqueeze(0), context)
                 
                 output = self.out(decoder_output.squeeze(1))
                 seq_probs.append(output.unsqueeze(1))
