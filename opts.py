74f5a48e17dc4611de3c7e5bcca4184797cde975
Chenxiao Guan <1011zaozao@gmail.com>
2018-07-12 17:19:30 -0400

    debug

--- opts.py
+++ opts.py
@@ -151,7 +151,7 @@ def parse_opt():
         , help='The maximum video duration of the training dataset'
     )
     parser.add_argument(
-        '--childsum', type=bool, default=False,
+        '--child_sum', type=bool, default=False,
         help='whether to use the childsum unit to fuse the hidden state'
     )
     parser.add_argument(
